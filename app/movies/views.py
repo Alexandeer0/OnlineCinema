0f35732d5e7def147ddf720d5086b4cf4bc94fd6
Alexandeer0 <a.m.kmit03@gmail.com>
2022-07-02 18:05:51 +0300

    Test 4

--- app/movies/views.py
+++ app/movies/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 def index_page(request):
     return HttpResponse(1000)
 
-def player_page(request, id):
-    return render(request, 'movies/player.htmlm', {'id': id})
+#def player_page(request, id):
+#    return render(request, 'movies/player.htmlm', {'id': id})
