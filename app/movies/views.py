c41992e5df9651ace2d9df5f46d09dcce4d587c8
Alexandeer0 <a.m.kmit03@gmail.com>
2022-07-02 19:02:32 +0300

    bd update 1

--- app/movies/views.py
+++ app/movies/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 from .models import Movie
-from django.http import HttpResponse
 
 
 def index_page(request):
     movies = Movie.objects.all()
-    print(movies)
-    return HttpResponse(300)
+    return render(request, 'movies/index.html', {'movies': movies})
 
 # def player_page(request, id):
 #    return render(request, 'movies/player.html', {'id': id})
