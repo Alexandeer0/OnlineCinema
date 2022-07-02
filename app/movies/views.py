8dc6520a2d3a63513802e48df9fd5c22fa1ce9ab
Alexandeer0 <a.m.kmit03@gmail.com>
2022-07-02 18:32:41 +0300

    Test 5

--- app/movies/views.py
+++ app/movies/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from .models import Movie
 from django.http import HttpResponse
 
-def index_page(request):
-    return HttpResponse(1000)
 
-#def player_page(request, id):
-#    return render(request, 'movies/player.htmlm', {'id': id})
+def index_page(request):
+    movies = Movie.objects.all()
+    print(movies)
+    return HttpResponse(300)
+
+# def player_page(request, id):
+#    return render(request, 'movies/player.html', {'id': id})
