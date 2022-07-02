c68ea8c54fb694d5d21ec37c395232fd5f04415d
Alexandeer0 <a.m.kmit03@gmail.com>
2022-07-02 19:20:42 +0300

    player view added

--- app/movies/urls.py
+++ app/movies/urls.py
@@ -1,6 +1,7 @@
-from .views import index_page
+from .views import index_page, player_page
 from django.urls import path
 
 urlpatterns = [
     path('', index_page),
+    path('movie/<int:kinopoisk_id>', player_page),
 ]
--- app/movies/views.py
+++ app/movies/views.py
@@ -6,5 +6,6 @@ def index_page(request):
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', {'movies': movies})
 
-# def player_page(request, id):
-#    return render(request, 'movies/player.html', {'id': id})
+
+def player_page(request, kinopoisk_id):
+    return render(request, 'movies/player.html', {'kinopoisk_id': kinopoisk_id})
