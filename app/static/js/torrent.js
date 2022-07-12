c1782a491c72b6486089a4af78676c1f48a9aa05
W1ld_P03n1x <W1ld.Ph03n1x@gmail.com>
2022-07-12 09:10:08 +0300

    Name and dascription

--- app/static/js/torrent.js
+++ app/static/js/torrent.js
@@ -1,17 +1,22 @@
 const client = new WebTorrent()
 
-const torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
+// const torrentId_TRUE = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
+const torrentId = document.getElementById("magnet").innerHTML.split("amp;").join("")+".torrent"
+// console.log(torrentId_TRUE)
+// console.log(torrentId_DIV)
+// console.log(torrentId_DIV.split("amp;").join("")==torrentId_TRUE)
+
 
 client.add(torrentId, function (torrent) {
+  console.log('we are here')
   // Torrents can contain many files. Let's use the .mp4 file
   const file = torrent.files.find(function (file) {
     return file.name.endsWith('.mp4')
   })
-  const poster = torrent.files.find(function (file) {
-    return file.name.endsWith('.jpg')
-  })
+  // const poster = torrent.files.find(function (file) {
+  //   return file.name.endsWith('.jpg')
+  // })
   // document.querySelector('video').poster = poster
-  
   file.renderTo('video')
   // file.getBlobURL(function (err, url) {
   //     if (err) return console.log(err.message)
